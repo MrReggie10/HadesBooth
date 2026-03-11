4aed7060e7bf4e92638aa48477f4ceb1af8a2e02
Reggie <jacobdyak@gmail.com>
2026-03-10 20:24:17 -0400

    Random updates (please pull)

--- Animatronic/Animatronic.ino
+++ Animatronic/Animatronic.ino
@@ -1,41 +1,72 @@
-/******************************************************************************
-**************************** HADESBOOTH ANIMATRONIC ***************************
-******************************************************************************/
+// Example sketch to control a stepper motor with A4988 stepper motor driver 
+// and Arduino without a library. 
+// https://www.makerguides.com
 
-/*
- Description:
-
-
- Created 2 Feb 2026
- by Jacob Yakubisin
-*/
-
-#include <Stepper.h>
-
-const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution
-// for your motor
-
-
-// initialize the stepper library on pins 8 through 11:
-Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
-
-int stepCount = 1;  // number of steps the motor has taken
-
-int motorSpeed = 0;
-int speedDelta = 1;
+// Define stepper motor connections and steps per revolution:
+#define dirPin 2
+#define stepPin 3
+#define stepsPerRevolution 200
 
 void setup() {
-  // nothing to do inside the setup
+  // Declare pins as output:
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
 }
 
 void loop() {
-  if(motorSpeed > 50 || motorSpeed <= 0) {
-    speedDelta *= -1;
+  // Set the spinning direction clockwise:
+  digitalWrite(dirPin, HIGH);
+
+  // Spin the stepper motor 1 revolution slowly:
+  for (int i = 0; i < stepsPerRevolution; i++) {
+    // These four lines result in 1 step:
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(2000);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(2000);
   }
-  motorSpeed += speedDelta;
-  // set the motor speed:
-  if (motorSpeed > 0) {
-    myStepper.setSpeed(motorSpeed);
-    myStepper.step(stepsPerRevolution / 100);
+
+  delay(1000);
+
+  // Set the spinning direction counterclockwise:
+  digitalWrite(dirPin, LOW);
+
+  // Spin the stepper motor 1 revolution quickly:
+  for (int i = 0; i < stepsPerRevolution; i++) {
+    // These four lines result in 1 step:
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(2000);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(2000);
   }
+
+  delay(1000);
+
+  // // Set the spinning direction clockwise:
+  // digitalWrite(dirPin, HIGH);
+
+  // // Spin the stepper motor 5 revolutions fast:
+  // for (int i = 0; i < 5 * stepsPerRevolution; i++) {
+  //   // These four lines result in 1 step:
+  //   digitalWrite(stepPin, HIGH);
+  //   delayMicroseconds(500);
+  //   digitalWrite(stepPin, LOW);
+  //   delayMicroseconds(500);
+  // }
+
+  // delay(1000);
+
+  // // Set the spinning direction counterclockwise:
+  // digitalWrite(dirPin, LOW);
+
+  // //Spin the stepper motor 5 revolutions fast:
+  // for (int i = 0; i < 5 * stepsPerRevolution; i++) {
+  //   // These four lines result in 1 step:
+  //   digitalWrite(stepPin, HIGH);
+  //   delayMicroseconds(500);
+  //   digitalWrite(stepPin, LOW);
+  //   delayMicroseconds(500);
+  // }
+
+  // delay(1000);
 }
