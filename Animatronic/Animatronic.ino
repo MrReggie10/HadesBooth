66a61a292b6c6c08ac56433e22ad6fd69da3303e
Reggie <jacobdyak@gmail.com>
2026-03-18 18:32:57 -0400

    Updated stepper rotation script

--- Animatronic/Animatronic.ino
+++ Animatronic/Animatronic.ino
@@ -14,7 +14,7 @@ void setup() {
 }
 
 void loop() {
-  // Set the spinning direction clockwise:
+  // Code to make flowers go down
   digitalWrite(dirPin, HIGH);
 
   // Spin the stepper motor 1 revolution slowly:
@@ -28,45 +28,18 @@ void loop() {
 
   delay(1000);
 
-  // Set the spinning direction counterclockwise:
-  digitalWrite(dirPin, LOW);
-
-  // Spin the stepper motor 1 revolution quickly:
-  for (int i = 0; i < stepsPerRevolution; i++) {
-    // These four lines result in 1 step:
-    digitalWrite(stepPin, HIGH);
-    delayMicroseconds(2000);
-    digitalWrite(stepPin, LOW);
-    delayMicroseconds(2000);
-  }
-
-  delay(1000);
-
-  // // Set the spinning direction clockwise:
-  // digitalWrite(dirPin, HIGH);
-
-  // // Spin the stepper motor 5 revolutions fast:
-  // for (int i = 0; i < 5 * stepsPerRevolution; i++) {
-  //   // These four lines result in 1 step:
-  //   digitalWrite(stepPin, HIGH);
-  //   delayMicroseconds(500);
-  //   digitalWrite(stepPin, LOW);
-  //   delayMicroseconds(500);
-  // }
-
-  // delay(1000);
-
-  // // Set the spinning direction counterclockwise:
+  // // Code to make flowers go up:
   // digitalWrite(dirPin, LOW);
 
-  // //Spin the stepper motor 5 revolutions fast:
-  // for (int i = 0; i < 5 * stepsPerRevolution; i++) {
+  // // Spin the stepper motor 1 revolution quickly:
+  // for (int i = 0; i < stepsPerRevolution; i++) {
   //   // These four lines result in 1 step:
   //   digitalWrite(stepPin, HIGH);
-  //   delayMicroseconds(500);
+  //   delayMicroseconds(2000);
   //   digitalWrite(stepPin, LOW);
-  //   delayMicroseconds(500);
+  //   delayMicroseconds(2000);
   // }
 
   // delay(1000);
+
 }
