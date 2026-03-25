5cc2957c7916fc62d8a9b83800ce5f6be11ee291
Reggie <jacobdyak@gmail.com>
2026-03-25 19:30:25 -0400

    Ardity added (has some issues)

---
+++ Game/Unity/HadesBooth/Assets/Scripts/LEDSending.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LEDSending : MonoBehaviour
+{
+    [SerializeField] private SerialController strip;
+
+    private int counter;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        counter = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(counter < 60)
+        {
+            yield return StartCoroutine(SendRed);
+        }
+        else if (counter < 120)
+        {
+            yield return StartCoroutine(SendBlue);
+        }
+        else if (counter < 180)
+        {
+            yield return StartCoroutine(SendYellow);
+        }
+        else
+        {
+            yield return StartCoroutine(SendCyan);
+        }
+
+        counter++;
+        if(counter >= 240)
+        {
+            counter = 0;
+        }
+    }
+
+    private IEnumerator SendRed()
+    {
+        strip.SendSerialMessage("r\n");
+        yield return 0;
+    }
+
+    private IEnumerator SendBlue()
+    {
+        strip.SendSerialMessage("b\n");
+        yield return 0;
+    }
+
+    private IEnumerator SendYellow()
+    {
+        strip.SendSerialMessage("y\n");
+        yield return 0;
+    }
+
+    private IEnumerator SendCyan()
+    {
+        strip.SendSerialMessage("c\n");
+        yield return 0;
+    }
+}
