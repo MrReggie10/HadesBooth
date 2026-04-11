1a599ad762d84061525c7b297c4b39b68ee577ee
Reggie <jacobdyak@gmail.com>
2026-04-11 10:56:58 -0400

    Bmode manual operation

--- Game/Unity/HadesBooth/Assets/Scripts/Arduino/ArduinoTesting.cs
+++ Game/Unity/HadesBooth/Assets/Scripts/Arduino/ArduinoTesting.cs
@@ -4,27 +4,40 @@ using UnityEngine;
 public class ArduinoTesting : MonoBehaviour
 {
     [SerializeField] public SerialController sender;
-    string[] validMessages = { "r", "y", "b", "c" };
     // This function is called by the Arduino MessageListner whenever any item is scanned.
-    public void HandleMessage(string message, int _)
+    void Update()
     {
-        message = message.Trim();
-        Debug.Log("Note Played: " + message);
-        if (message == "r")
+        if (Input.GetKeyDown(KeyCode.R))
         {
             sender.SendSerialMessage("L 12 0 0 r 200 0 k 200 1 r 400 1 k 400 2 r 600 2 k 600 3 r 800 3 k 800 4 r 1000 4 k 1000 5 r 1200 5 k");
         }
-        else if (message == "b")
+        else if (Input.GetKeyDown(KeyCode.B))
         {
             sender.SendSerialMessage("L 12 0 0 b 200 0 k 200 1 b 400 1 k 400 2 b 600 2 k 600 3 b 800 3 k 800 4 b 1000 4 k 1000 5 b 1200 5 k");
         }
-        else if (message == "c")
+        else if (Input.GetKeyDown(KeyCode.C))
         {
             sender.SendSerialMessage("L 12 0 0 c 200 0 k 200 1 c 400 1 k 400 2 c 600 2 k 600 3 c 800 3 k 800 4 c 1000 4 k 1000 5 c 1200 5 k");
         }
-        else if (message == "y")
+        else if (Input.GetKeyDown(KeyCode.Y))
         {
             sender.SendSerialMessage("L 12 0 0 y 200 0 k 200 1 y 400 1 k 400 2 y 600 2 k 600 3 y 800 3 k 800 4 y 1000 4 k 1000 5 y 1200 5 k");
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            sender.SendSerialMessage("5");
+        }
+        else if (Input.GetKeyDown(KeyCode.U))
+        {
+            sender.SendSerialMessage("1");
+        }
+        else if (Input.GetKeyDown(KeyCode.O))
+        {
+            sender.SendSerialMessage("u");
+        }
+        else if (Input.GetKeyDown(KeyCode.P))
+        {
+            sender.SendSerialMessage("d");
         }
     }
 
--- Game/Unity/HadesBooth/Assets/Scripts/Arduino/MessageListener.cs
+++ Game/Unity/HadesBooth/Assets/Scripts/Arduino/MessageListener.cs
@@ -8,47 +8,47 @@
 
 // Modified by Taylor Roberts
 
-using UnityEngine;
-using System.Collections;
+// using UnityEngine;
+// using System.Collections;
 
-/**
- * When creating your message listeners you need to implement these two methods:
- *  - OnMessageArrived
- *  - OnConnectionEvent
- */
-public class MessageListener : MonoBehaviour
-{
-    private enum ArduinoType { Lyre, Guitar, Conductor, FlowerWall }
-    [SerializeField] private ArduinoType arduinoType;
-    [SerializeField] private GameStatus gameStatus;
-    [SerializeField] private ArduinoTesting arduinoTester;
+// /**
+//  * When creating your message listeners you need to implement these two methods:
+//  *  - OnMessageArrived
+//  *  - OnConnectionEvent
+//  */
+// public class MessageListener : MonoBehaviour
+// {
+//     private enum ArduinoType { Lyre, Guitar, Conductor, FlowerWall }
+//     [SerializeField] private ArduinoType arduinoType;
+//     [SerializeField] private GameStatus gameStatus;
+//     [SerializeField] private ArduinoTesting arduinoTester;
     
 
-    // Invoked when a line of data is received from the serial device.
-    public void OnMessageArrived(string msg)
-    {
-        Debug.Log(arduinoType.ToString() + ": " + msg);
-        if (gameStatus != null)
-        {
-            if (arduinoType == ArduinoType.Lyre) gameStatus.HandleMessage(msg, 0);
-            else if (arduinoType == ArduinoType.Guitar) gameStatus.HandleMessage(msg, 1);
-        }
+//     // Invoked when a line of data is received from the serial device.
+//     public void OnMessageArrived(string msg)
+//     {
+//         Debug.Log(arduinoType.ToString() + ": " + msg);
+//         if (gameStatus != null)
+//         {
+//             if (arduinoType == ArduinoType.Lyre) gameStatus.HandleMessage(msg, 0);
+//             else if (arduinoType == ArduinoType.Guitar) gameStatus.HandleMessage(msg, 1);
+//         }
         
-        else // B mode
-        {
-            if (arduinoType == ArduinoType.Lyre) arduinoTester.HandleMessage(msg, 0);
-            else if (arduinoType == ArduinoType.Guitar) arduinoTester.HandleMessage(msg, 1);
-        }
-    }
+//         else // B mode
+//         {
+//             if (arduinoType == ArduinoType.Lyre) arduinoTester.HandleMessage(msg, 0);
+//             else if (arduinoType == ArduinoType.Guitar) arduinoTester.HandleMessage(msg, 1);
+//         }
+//     }
 
-    // Invoked when a connect/disconnect event occurs. The parameter 'success'
-    // will be 'true' upon connection, and 'false' upon disconnection or
-    // failure to connect.
-    public void OnConnectionEvent(bool success)
-    {
-        if (success)
-            Debug.Log("Connection established");
-        else
-            Debug.Log("Connection attempt failed or disconnection detected");
-    }
-}
+//     // Invoked when a connect/disconnect event occurs. The parameter 'success'
+//     // will be 'true' upon connection, and 'false' upon disconnection or
+//     // failure to connect.
+//     public void OnConnectionEvent(bool success)
+//     {
+//         if (success)
+//             Debug.Log("Connection established");
+//         else
+//             Debug.Log("Connection attempt failed or disconnection detected");
+//     }
+// }
